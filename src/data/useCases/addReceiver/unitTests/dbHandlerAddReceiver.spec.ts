fb6806c43f269a42a1569c0627c4e3cced84441e
RicardoMorato <rmr@cin.ufpe.br>
2023-03-04 11:41:46 -0300

    test: should call repository with correct fields

--- src/data/useCases/addReceiver/unitTests/dbHandlerAddReceiver.spec.ts
+++ src/data/useCases/addReceiver/unitTests/dbHandlerAddReceiver.spec.ts
@@ -1,23 +1,40 @@
 import { makeSut } from "./testUtils";
 
 describe("dbHandlerAddReceiver use case", () => {
+  const receiverData = {
+    name: "valid_name",
+    email: "valid_email",
+    document: "11111111111",
+    pixKeyType: "EMAIL",
+    pixKey: "valid_email",
+  };
+
+  const receiverDataEncrypted = {
+    name: "valid_name",
+    email: "encrypted_value",
+    document: "encrypted_value",
+    pixKeyType: "encrypted_value",
+    pixKey: "encrypted_value",
+  };
+
   it("should call Encrypter with correct fields", async () => {
     const { sut, encrypterStub } = makeSut();
     const encryptSpy = jest.spyOn(encrypterStub, "encrypt");
 
-    const receiverData = {
-      name: "valid_name",
-      email: "valid_email",
-      document: "11111111111",
-      pixKeyType: "EMAIL",
-      pixKey: "valid_email",
-    };
+    await sut.add(receiverData);
+
+    expect(encryptSpy).toHaveBeenCalledWith(receiverData.email);
+    expect(encryptSpy).toHaveBeenCalledWith(receiverData.document);
+    expect(encryptSpy).toHaveBeenCalledWith(receiverData.pixKeyType);
+    expect(encryptSpy).toHaveBeenCalledWith(receiverData.pixKey);
+  });
+
+  it("should call AddReceiverRepository with correct values", async () => {
+    const { sut, addReceiverRepositoryStub } = makeSut();
+    const repositorySpy = jest.spyOn(addReceiverRepositoryStub, "add");
 
     await sut.add(receiverData);
 
-    expect(encryptSpy).toHaveBeenCalledWith("valid_email");
-    expect(encryptSpy).toHaveBeenCalledWith("11111111111");
-    expect(encryptSpy).toHaveBeenCalledWith("EMAIL");
-    expect(encryptSpy).toHaveBeenCalledWith("valid_email");
+    expect(repositorySpy).toHaveBeenCalledWith(receiverDataEncrypted);
   });
 });
