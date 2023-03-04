264b9a20243125aa0217bccefae79a5dca5b608d
RicardoMorato <rmr@cin.ufpe.br>
2023-03-04 16:58:34 -0300

    test: controller for receiver creation

---
+++ src/presentation/controllers/createReceiver/tests/createReceiver.spec.ts
@@ -0,0 +1,71 @@
+import { ServerError } from "@/presentation/errors";
+import { makeSut } from "./testUtils";
+
+describe("createReceiverController", () => {
+  const receiverData = {
+    name: "valid_name",
+    email: "VALID-EMAIL@MAIL.COM",
+    document: "11111111111",
+    pixKeyType: "EMAIL",
+    pixKey: "VALID-EMAIL@MAIL.COM",
+  };
+
+  const request = {
+    body: receiverData,
+  };
+
+  it("should call validator adapter with correct values", async () => {
+    const { sut, validatorAdapterStub } = makeSut();
+    const validateSpy = jest.spyOn(validatorAdapterStub, "validate");
+
+    await sut.handle(request);
+
+    expect(validateSpy).toHaveBeenCalledWith(request);
+  });
+
+  it("should return 500 if validator adapter throws", async () => {
+    const { sut, validatorAdapterStub } = makeSut();
+    jest.spyOn(validatorAdapterStub, "validate").mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const response = await sut.handle(request);
+
+    expect(response.statusCode).toBe(500);
+    expect(response.body).toEqual(new ServerError(""));
+  });
+
+  it("should call add receiver with correct values", async () => {
+    const { sut, addReceiverStub } = makeSut();
+    const addReceiverSpy = jest.spyOn(addReceiverStub, "add");
+
+    await sut.handle(request);
+
+    expect(addReceiverSpy).toHaveBeenCalledWith(request.body);
+  });
+
+  it("should return 500 if add receiver throws", async () => {
+    const { sut, addReceiverStub } = makeSut();
+    jest.spyOn(addReceiverStub, "add").mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const response = await sut.handle(request);
+
+    expect(response.statusCode).toBe(500);
+    expect(response.body).toEqual(new ServerError(""));
+  });
+
+  it("should return 200 if valid data is provided", async () => {
+    const { sut } = makeSut();
+
+    const response = await sut.handle(request);
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual({
+      ...request.body,
+      id: "valid_id",
+      status: "RASCUNHO",
+    });
+  });
+});
