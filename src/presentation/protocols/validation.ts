cc3cb8e192030c780e4c8566e23491af1d51635d
RicardoMorato <rmr@cin.ufpe.br>
2023-03-04 14:53:33 -0300

    feat: add validator protocol for presentation layer

--- src/presentation/protocols/validation.ts
+++ src/presentation/protocols/validation.ts
@@ -1,3 +1,12 @@
-export interface Validation {
-  validate: (input: any) => Error;
+import { PixKeyType } from "@/domain/models/Receiver";
+import { ValidationResult } from "@/presentation/utils/createReceiverValidatorAdapter";
+import { HttpRequest } from "./http";
+
+export interface CreateReceiverValidation {
+  validate: (request: HttpRequest) => ValidationResult;
+  isNameValid: (name: string) => boolean;
+  isEmailValid: (email: string) => boolean;
+  isDocumentValid: (document: string) => boolean;
+  isPixKeyTypeValid: (pixKeyType: PixKeyType | string) => boolean;
+  isPixKeyValid: (pixKey: string, pixKeyType: string) => boolean;
 }
