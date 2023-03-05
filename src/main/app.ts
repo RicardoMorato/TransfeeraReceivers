3f6330d52869392c9807900bcb32bf37a0eba79d
RicardoMorato <rmr@cin.ufpe.br>
2023-03-05 11:34:38 -0300

    feat: setup database connection

--- src/main/app.ts
+++ src/main/app.ts
@@ -2,15 +2,13 @@ import "module-alias/register";
 import express, { Application } from "express";
 import dotenv from "dotenv";
 
-import { DBHelper as Database } from "@/infra/db";
+import { database } from "@/infra/db";
 
 dotenv.config();
 
 const app: Application = express();
 
 const port: string | number = process.env.PORT || 8080;
-
-const database = new Database(process.env.DB_CONNECTION_URL);
 
 database
   .connect()
