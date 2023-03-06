7ae33d6e1de60feb44ae298ca6230c8182bfc10f
RicardoMorato <rmr@cin.ufpe.br>
2023-03-06 10:45:17 -0300

    feat: validate params using validator adapter

--- src/presentation/controllers/listAllReceivers/listAllReceivers.ts
+++ src/presentation/controllers/listAllReceivers/listAllReceivers.ts
@@ -1,20 +1,34 @@
 import { ListReceivers } from "@/domain/useCases/listReceivers";
-import { ok, serverError } from "@/presentation/responseSchemas/http-helper";
+import {
+  badRequest,
+  ok,
+  serverError,
+} from "@/presentation/responseSchemas/http-helper";
 import {
   Controller,
   HttpRequest,
   HttpResponse,
+  ListAllReceiversValidation,
 } from "@/presentation/protocols";
 
 export class ListAllReceiversController implements Controller {
   private readonly listReceivers: ListReceivers;
+  private readonly validatorAdapter: ListAllReceiversValidation;
 
-  constructor(listReceivers: ListReceivers) {
+  constructor(
+    listReceivers: ListReceivers,
+    validatorAdapter: ListAllReceiversValidation
+  ) {
     this.listReceivers = listReceivers;
+    this.validatorAdapter = validatorAdapter;
   }
 
   async handle(request: HttpRequest): Promise<HttpResponse> {
     try {
+      const { isValid, error } = this.validatorAdapter.validate(request);
+
+      if (!isValid) return badRequest(error);
+
       const receivers = await this.listReceivers.list();
 
       return ok({ data: receivers });
