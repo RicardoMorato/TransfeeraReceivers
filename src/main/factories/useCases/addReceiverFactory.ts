e8e2705d6b308f29be3bc8791fd161870ee7c1e1
RicardoMorato <rmr@cin.ufpe.br>
2023-03-06 19:10:11 -0300

    refactor: change encryption driver on factories

--- src/main/factories/useCases/addReceiverFactory.ts
+++ src/main/factories/useCases/addReceiverFactory.ts
@@ -1,11 +1,11 @@
 import { DbHandlerAddReceiver } from "@/data/useCases/addReceiver/dbHandlerAddReceiver";
 import { AddReceiver } from "@/domain/useCases/addReceiver";
 import { ReceiverMongoRepository } from "@/infra/db";
-import { CryptrAdapter } from "@/infra/cryptography";
+import { CryptrAdapter, DumbEncrypterAdapter } from "@/infra/cryptography";
 
 export const makeDbAddReceiver = (): AddReceiver => {
-  const cryptrAdapter = new CryptrAdapter();
+  const cryptoAdapter = new DumbEncrypterAdapter();
   const receiverMongoRepository = new ReceiverMongoRepository();
 
-  return new DbHandlerAddReceiver(cryptrAdapter, receiverMongoRepository);
+  return new DbHandlerAddReceiver(cryptoAdapter, receiverMongoRepository);
 };
--- src/main/factories/useCases/listReceiversFactory.ts
+++ src/main/factories/useCases/listReceiversFactory.ts
@@ -1,11 +1,11 @@
 import { DbHandlerListReceivers } from "@/data/useCases/listReceivers/dbHandlerListReceivers";
 import { ListReceivers } from "@/domain/useCases/listReceivers";
 import { ReceiverMongoRepository } from "@/infra/db";
-import { CryptrAdapter } from "@/infra/cryptography";
+import { CryptrAdapter, DumbEncrypterAdapter } from "@/infra/cryptography";
 
 export const makeDbListReceivers = (): ListReceivers => {
-  const cryptrAdapter = new CryptrAdapter();
+  const cryptoAdapter = new DumbEncrypterAdapter();
   const receiverMongoRepository = new ReceiverMongoRepository();
 
-  return new DbHandlerListReceivers(cryptrAdapter, receiverMongoRepository);
+  return new DbHandlerListReceivers(cryptoAdapter, receiverMongoRepository);
 };
