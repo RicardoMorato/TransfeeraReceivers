790193d8f0382383cf16c1dc01068c61a611c593
RicardoMorato <rmr@cin.ufpe.br>
2023-03-06 10:36:40 -0300

    feat: list all receivers in repository

--- src/data/protocols/receiverRepository.ts
+++ src/data/protocols/receiverRepository.ts
@@ -1,6 +1,11 @@
 import { Document, InsertOneResult } from "mongodb";
 import { AddReceiverModel } from "@/domain/useCases/addReceiver";
+import { ReceiverModel } from "@/domain/models";
 
 export interface AddReceiverRepository {
   add(receiver: AddReceiverModel): Promise<InsertOneResult<Document>>;
+}
+
+export interface ListReceiverRepository {
+  list(): Promise<ReceiverModel[]>;
 }
--- src/infra/db/mongodb/receiverMongoRepository.ts
+++ src/infra/db/mongodb/receiverMongoRepository.ts
@@ -1,12 +1,27 @@
 import { InsertOneResult, Document } from "mongodb";
 import { database } from "@/infra/db";
-import { AddReceiverRepository } from "@/data/protocols/receiverRepository";
+import {
+  AddReceiverRepository,
+  ListReceiverRepository,
+} from "@/data/protocols/receiverRepository";
 import { AddReceiverModel } from "@/domain/useCases/addReceiver";
+import { ReceiverModel } from "@/domain/models";
 
-export class ReceiverMongoRepository implements AddReceiverRepository {
+export class ReceiverMongoRepository
+  implements AddReceiverRepository, ListReceiverRepository
+{
   async add(receiver: AddReceiverModel): Promise<InsertOneResult<Document>> {
     const receiverCollection = database.getCollection("receivers");
     const result = await receiverCollection.insertOne(receiver);
+
+    return result;
+  }
+
+  async list(): Promise<ReceiverModel[]> {
+    const receiverCollection = database.getCollection("receivers");
+    const cursor = receiverCollection.find<ReceiverModel>({});
+
+    const result = await cursor.toArray();
 
     return result;
   }
